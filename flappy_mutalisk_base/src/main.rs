9c5525a766e377fb1034b16f5c570a5ebcfc0a69
Alec <alhampton9@gmail.com>
2026-05-24 16:29:47 -0600

    Added gravity and flapping. Changed scale.

--- flappy_mutalisk_base/src/main.rs
+++ flappy_mutalisk_base/src/main.rs
@@ -3,14 +3,14 @@ use my_library::*;
 
 const WINDOW_WIDTH: u32 = 1024;
 const WINDOW_HEIGHT: u32 = 768;
-const SCALE_FACTOR: f32 = 6.0;
+const SCALE_FACTOR: f32 = 4.0;
 
 const SPRITE_WIDTH_PIXELS:f32 = 8.0;
 const SPRITE_HEIGHT_PIXELS:f32 = 8.0;
 
-const VIRTUAL_WIDTH: f32 = WINDOW_WIDTH as f32 / SCALE_FACTOR;
-const VIRTUAL_HEIGHT: f32 = WINDOW_HEIGHT as f32 / SCALE_FACTOR;
-const WORLD_TOP: f32 = VIRTUAL_HEIGHT / 2.0;
+// const VIRTUAL_WIDTH: f32 = WINDOW_WIDTH as f32 / SCALE_FACTOR;
+// const VIRTUAL_HEIGHT: f32 = WINDOW_HEIGHT as f32 / SCALE_FACTOR;
+// const WORLD_TOP: f32 = VIRTUAL_HEIGHT / 2.0;
 
 #[derive(Component)]
 struct Player {
@@ -39,6 +39,8 @@ fn main() {
         }).set(ImagePlugin::default_nearest()))
         .add_plugins(RandomPlugin)
         .add_systems(Startup, setup)
+        .add_systems(Update, gravity)
+        .add_systems(Update, flap)
         .add_systems(Update, draw_debug)
         .run();
 }
@@ -50,10 +52,10 @@ fn setup(
     mut drawer: Gizmos,
     mut rng: ResMut<RandomNumberGenerator>,
 ) {
-    // Camera
+    // Spawn camera
     commands.spawn(Camera2d);
 
-
+    // Spawn player
     let player_sprite_sheet = asset_server.load("Dream-Catcher-enemies.png");
     let player_sprite_layout = TextureAtlasLayout::from_grid(UVec2::splat(8), 10, 21, None, None);
     let texture_atlas_layout = texture_atlas_layouts.add(player_sprite_layout);
@@ -63,10 +65,12 @@ fn setup(
             index: 10,
         });
     commands.spawn((
+        Player { gravity: 0.0 },
         player_sprite,
-        Transform::from_xyz(-55.0, 0.0, 1.0),
+        Transform::from_xyz(-13.0 * SPRITE_WIDTH_PIXELS, 0.0, 1.0),
     ));
 
+    // Spawn wall
     let wall_sprite_sheet = asset_server.load("Dream-Catcher-ground-wall.png");
     let wall_sprite_layout = TextureAtlasLayout::from_grid(UVec2::splat(8), 32, 30, None, None);
     let texture_atlas_layout = texture_atlas_layouts.add(wall_sprite_layout);
@@ -76,7 +80,44 @@ fn setup(
             index: 439,
         });
  
-    build_wall(&mut commands, &wall_sprite, 8);
+    build_wall(&mut commands, &wall_sprite, 0);
+}
+
+fn build_wall(
+    commands: &mut Commands,
+    wall_sprite: &Sprite,
+    gap_center_y: i32,
+) {
+    // commands.spawn((
+    //     wall_sprite.clone(),
+    //     Transform::from_xyz(0.0, 0.0, 1.0),
+    //     Obstacle,
+    // ));
+
+    for y in -11..=11{
+        if y < gap_center_y - 2 || y > gap_center_y + 2 {
+            commands.spawn((
+                wall_sprite.clone(),
+                Transform::from_xyz(-4.0 * SPRITE_WIDTH_PIXELS, y as f32 * 8.0, 1.0),
+                Obstacle,
+            ));
+        }
+    }
+}
+
+fn gravity(mut query: Query<(&mut Player, &mut Transform)>) { 
+    if let Ok((mut player, mut transform)) = query.single_mut() {
+        player.gravity += 0.1;
+        transform.translation.y -= player.gravity;
+    }
+}
+
+fn flap(keyboard: Res<ButtonInput<KeyCode>>, mut query: Query<&mut Player>) {
+    if keyboard.pressed(KeyCode::Space) {
+        if let Ok(mut player) = query.single_mut() {
+            player.gravity = -1.8;
+        }
+    }
 }
 
 fn draw_debug(mut gizmos: Gizmos) {
@@ -87,27 +128,4 @@ fn draw_debug(mut gizmos: Gizmos) {
         Vec2::new(SPRITE_WIDTH_PIXELS, SPRITE_HEIGHT_PIXELS),
         LinearRgba::RED.with_alpha(0.1),
     ).outer_edges();
-}
-
-fn build_wall(
-    commands: &mut Commands,
-    wall_sprite: &Sprite,
-    gap_y: i32,
-) {
-    println!("WORLD TOP!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!: {WORLD_TOP}");
-    commands.spawn((
-        wall_sprite.clone(),
-        Transform::from_xyz(0.0, 0.0, 1.0),
-        Obstacle,
-    ));
-
-    for y in -12..=-12{
-        if y < gap_y - 4 || y > gap_y + 4 {
-            commands.spawn((
-                wall_sprite.clone(),
-                Transform::from_xyz(45.0, y as f32 * 8.0, 1.0),
-                Obstacle,
-            ));
-        }
-    }
 }
