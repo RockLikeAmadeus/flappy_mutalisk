1f227313fae040088edbca610f243038dd1b3e23
Alec <alhampton9@gmail.com>
2026-05-24 15:48:03 -0600

    Drawing a grid so I can orient myself

--- flappy_mutalisk_base/src/main.rs
+++ flappy_mutalisk_base/src/main.rs
@@ -1,9 +1,12 @@
-use bevy::{app::AppExit, prelude::*, window::WindowResolution};
+use bevy::{app::AppExit, gizmos, prelude::*, window::WindowResolution};
 use my_library::*;
 
 const WINDOW_WIDTH: u32 = 1024;
 const WINDOW_HEIGHT: u32 = 768;
 const SCALE_FACTOR: f32 = 6.0;
+
+const SPRITE_WIDTH_PIXELS:f32 = 8.0;
+const SPRITE_HEIGHT_PIXELS:f32 = 8.0;
 
 const VIRTUAL_WIDTH: f32 = WINDOW_WIDTH as f32 / SCALE_FACTOR;
 const VIRTUAL_HEIGHT: f32 = WINDOW_HEIGHT as f32 / SCALE_FACTOR;
@@ -29,13 +32,14 @@ fn main() {
             primary_window: Some(Window {
                 title: "Flappy Mutalisk".into(),
                 resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT)
-                    .with_scale_factor_override(1.0),//SCALE_FACTOR),
+                    .with_scale_factor_override(SCALE_FACTOR),
                 ..default()
             }),
             ..default()
         }).set(ImagePlugin::default_nearest()))
         .add_plugins(RandomPlugin)
         .add_systems(Startup, setup)
+        .add_systems(Update, draw_debug)
         .run();
 }
 
@@ -43,9 +47,12 @@ fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+    mut drawer: Gizmos,
     mut rng: ResMut<RandomNumberGenerator>,
 ) {
+    // Camera
     commands.spawn(Camera2d);
+
 
     let player_sprite_sheet = asset_server.load("Dream-Catcher-enemies.png");
     let player_sprite_layout = TextureAtlasLayout::from_grid(UVec2::splat(8), 10, 21, None, None);
@@ -72,6 +79,16 @@ fn setup(
     build_wall(&mut commands, &wall_sprite, 8);
 }
 
+fn draw_debug(mut gizmos: Gizmos) {
+    // Draw a grid, so we can help orient ourselves
+    gizmos.grid_2d(
+        Isometry2d::IDENTITY,
+        UVec2::new(100, 100),
+        Vec2::new(SPRITE_WIDTH_PIXELS, SPRITE_HEIGHT_PIXELS),
+        LinearRgba::RED.with_alpha(0.1),
+    ).outer_edges();
+}
+
 fn build_wall(
     commands: &mut Commands,
     wall_sprite: &Sprite,
@@ -80,7 +97,7 @@ fn build_wall(
     println!("WORLD TOP!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!: {WORLD_TOP}");
     commands.spawn((
         wall_sprite.clone(),
-        Transform::from_xyz(0.0, WINDOW_HEIGHT as f32 / 2.0 - 8.0, 1.0),
+        Transform::from_xyz(0.0, 0.0, 1.0),
         Obstacle,
     ));
 
